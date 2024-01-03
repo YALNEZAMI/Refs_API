12fc952f4ac5ccac08d1dd7bd6c3bfc221306156
YALNEZAMI <yaseralnezami@gmail.com>
2024-01-03 04:23:45 +0100

    comments first commit

--- src/services/index.js
+++ src/services/index.js
@@ -1,8 +1,10 @@
 /* eslint-disable linebreak-style */
 const users = require("./users/users.service.js");
 const refs = require("./refs/refs.service.js");
+const comments = require("./comments/comments.service.js");
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
   app.configure(refs);
+  app.configure(comments);
 };
